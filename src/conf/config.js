c296d16c003aefa07513d18db9209bc4dfc1b1f1
Priyanshu Saraswat <saraswatpriyanshu29@gmail.com>
2024-11-14 11:21:56 +0530

    Create config.js

---
+++ src/conf/config.js
@@ -0,0 +1,70 @@
+import conf from "./conf";
+import { Client, ID, Databases, Storage, Query} from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectiontId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+            
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error", error );
+            
+        }
+    }
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectiontId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+            
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error", error );
+            
+        }
+    }
+    async deleteDocument(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectiontId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            
+        }
+    }
+}
+
+export default Service;
